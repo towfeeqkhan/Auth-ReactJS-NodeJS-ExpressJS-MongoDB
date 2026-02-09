b9906909c41b29314fd32fa54c3fcdd1e2f9dbb7
Towfeeq Khan <ask.towfeeq@gmail.com>
2026-02-09 18:40:43 +0530

    add user model with schema definition for authentication

---
+++ models/user.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 3 },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
